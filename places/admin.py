04650345ee312876bfdca7f4737be0753644a5db
gardnervile <lordy1jo@gmail.com>
2025-04-17 19:37:29 +0500

    add place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Place
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ("title", "latitude", "longitude")
 # Register your models here.
--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-print("hello!")
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField(blank=True)
+    description_long = models.TextField(blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.title
 # Create your models here.
