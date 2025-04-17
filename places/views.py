3975763ebaf0d7fcebff58c8570683796d050fcb
gardnervile <lordy1jo@gmail.com>
2025-04-17 20:23:28 +0500

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Place
+import json
 
-# Create your views here.
+def show_places(request):
+    places = Place.objects.all()
+    
+    places_data = []
+    for place in places:
+        places_data.append({
+            'name': place.name,
+            'latitude': place.latitude,
+            'longitude': place.longitude,
+            'image_url': place.image.url,
+        })
+    
+    return render(request, 'index.html', {'places_data': json.dumps(places_data)})
+
